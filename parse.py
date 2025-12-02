58564b30e049ef89683abc6f9eb536efb35cbab1
Carmen-Git-It <carmenwhitton@gmail.com>
2025-12-02 18:20:07 +0000

    feat: table and choices populating

--- parse.py
+++ parse.py
@@ -40,12 +40,20 @@ for update in root.findall('.//sys_update_xml'):
         created_by = inner_root.findtext('.//sys_created_by').strip()
         updated_by = inner_root.findtext('.//sys_updated_by')
         display_name = inner_root.findtext('.//sys_name')
+        choice_list = []
         internal_type_el = inner_root.find('.//internal_type')
         internal_type = None
         if internal_type_el is not None:
             internal_type = internal_type_el.get('display_value')
             if not internal_type:
                 internal_type = internal_type_el.text
+
+        if type == 'Choice list':
+            choices = inner_root.find('.//sys_choice')
+            for choice in choices:
+                label = choice.findtext('.//label')
+                if label is not None and label != '':
+                    choice_list.append(label)
 
         if table is None or table == '':
             table = inner_root.get('table')
@@ -67,14 +75,14 @@ for update in root.findall('.//sys_update_xml'):
         if column_label:
             display_name = column_label
 
-        elements[sys_id] = [type or 'None', action or 'None', table or 'None', name or 'None', display_name or 'None', sys_id or 'None', scope or 'None', updated_by or 'None', updated or 'None', internal_type or 'None']
+        elements[sys_id] = [type or 'None', action or 'None', table or 'None', name or 'None', display_name or 'None', sys_id or 'None', scope or 'None', updated_by or 'None', updated or 'None', internal_type or 'None', choice_list or 'None']
 
     except ET.XMLSyntaxError as e:
         print('invalid payload: ', e)
 
 with open('output.csv', 'w') as f:
     writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
-    f.write('Type,Action,Table,Name,Display Name,Sys ID,Scope,Updated By, Updated On, Internal Type\n')
+    f.write('Type,Action,Table,Name,Display Name,Sys ID,Scope,Updated By, Updated On, Internal Type, Choice List\n')
     for el in elements:
         if elements[el] is not None:
             writer.writerow(elements[el])
